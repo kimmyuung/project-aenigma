3b7ca9ba0f00459e53a43ffed2ec395088a6748a
김명호 <kimmyungho1003@naver.com>
2025-07-10 21:34:03 +0900

    project init

---
+++ voucher/src/main/java/kmh/project/voucher/app/controller/request/EmployeeRequest.java
@@ -0,0 +1,4 @@
+package kmh.project.voucher.app.controller.request;
+
+public record EmployeeRequest(String name, String position, String department) {
+}
---
+++ voucher/src/main/java/kmh/project/voucher/app/controller/response/EmployeeResponse.java
@@ -0,0 +1,4 @@
+package kmh.project.voucher.app.controller.response;
+
+public record EmployeeResponse(Long no, String name, String position, String department) {
+}
