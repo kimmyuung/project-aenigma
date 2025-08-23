314028922cf68934b279840166f9acf12c04de4d
김명호 <kimmyungho1003@naver.com>
2025-08-23 20:35:56 +0900

    ai project

---
+++ aiproject/src/main/java/kmh/dev/aiproject/rest/SimpleChatController.java
@@ -0,0 +1,20 @@
+package kmh.dev.aiproject.rest;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleChatController {
+
+    private final ChatClient chatClient;
+
+    public SimpleChatController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("ai")
+    String generation(String userPrompt) {
+        return this.chatClient.prompt().user(userPrompt).call().content();
+    }
+}
